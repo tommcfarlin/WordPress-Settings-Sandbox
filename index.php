5c49fd3a064729ee5d461a63f08bb68bf6364b6b
Tom McFarlin <tom@tommcfarlin.com>
2013-01-16 08:46:45 -0500

    Fixing some minor offset errors in the index template

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 		<?php $social_options = get_option ( 'sandbox_theme_social_options' ); ?>
 		<?php $input_examples = get_option('sandbox_theme_input_examples'); ?>
 	
-		<?php if( $display_options[ 'show_header' ] ) { ?>
+		<?php if( isset( $display_options['show_header'] ) && $display_options[ 'show_header' ] ) { ?>
 			<div id="header">
 				<h1>Sandbox Header</h1>
 			</div><!-- /#header -->
 		<?php } // end if ?>
 		
-		<?php if( $display_options[ 'show_content' ] ) { ?>
+		<?php if( isset( $display_options['show_content'] ) && $display_options[ 'show_content' ] ) { ?>
 			<div id="content">
 				<?php echo $social_options['twitter'] ? '<a href="' . esc_url( $social_options['twitter'] ) . '">Twitter</a>' : ''; ?>
 				<?php echo $social_options['facebook'] ? '<a href="' . esc_url( $social_options['facebook'] ) . '">Facebook</a>' : ''; ?>
@@ -23,37 +23,37 @@
 			</div><!-- /#content -->
 		<?php } // end if ?>
 		
-		<?php if( $display_options['show_footer'] ) { ?>
+		<?php if( isset( $display_options['show_footer'] ) && $display_options['show_footer'] ) { ?>
 			<div id="footer">
 				<p>&copy; <?php echo date( 'Y' ); ?> All Rights Reserved.</p>
 			</div><!-- /#footer -->
 		<?php } // end if ?>
 
-		<?php if( $input_examples['input_example'] ) { ?>
+		<?php if( isset( $input_examples['input_example'] ) && $input_examples['input_example'] ) { ?>
 			<?php echo sanitize_text_field( $input_examples['input_example'] ); ?>
 		<?php } // end if ?>
 
-		<?php if( $input_examples['textarea_example'] ) { ?>
+		<?php if( isset( $input_examples['textarea_example'] ) &&  $input_examples['textarea_example'] ) { ?>
 			<?php echo sanitize_text_field( $input_examples['textarea_example'] ); ?>
 		<?php } // end if ?>
 		
-		<?php if( $input_examples['checkbox_example'] == '1' ) { ?>
+		<?php if( '1' == $input_examples['checkbox_example'] ) { ?>
 			<p>The checkbox has been checked.</p>
 		<?php } else { ?>
 			<p>The checkbox has not been checked.</p>
 		<?php } // end if  ?>
 		
-		<?php if( $input_examples['radio_example'] == 1 ) { ?>
+		<?php if( 1 == $input_examples['radio_example'] ) { ?>
 			<p>The first option was selected.</p>
-		<?php } elseif( $input_examples['radio_example'] == 2 ) { ?>
+		<?php } elseif( 2 == $input_examples['radio_example'] ) { ?>
 			<p>The second option was selected.</p>
 		<?php } // end if  ?>
 		
-		<?php if( $input_examples['time_options'] == 'never' ) { ?>
+		<?php if( 'never' == $input_examples['time_options'] ) { ?>
 			<p>Never display this. Somewhat ironic to even show this.</p>
-		<?php } elseif( $input_examples['time_options'] == 'sometimes' ) { ?>
+		<?php } elseif( 'sometimes' == $input_examples['time_options'] ) { ?>
 			<p>Sometimes display this.</p>
-		<?php } elseif( $input_examples['time_options'] == 'always' ) { ?>
+		<?php } elseif( 'always' == $input_examples['time_options'] ) { ?>
 			<p>Always display this.</p>
 		<?php } // end if/else ?>
 	
